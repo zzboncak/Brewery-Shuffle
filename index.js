c93256bb6f62795220ec18e6e162850e894df2f8
Zachary <zachary.zboncak@gmail.com>
2019-10-14 00:33:08 -0500

    beginning to plug google results into open brewery db api

--- index.js
+++ index.js
@@ -8,33 +8,42 @@ function watchForm() {
             alert(`Please input a Zip code`);
         }else{
             //this is where the API will be called
+            initializeMap();
             renderMap(userInput);
+            //console.log(breweryName);
         }
     })
 }
 
 let url = `https://api.openbrewerydb.org/breweries/2394`;
-let brewery;
-let longitude;
-let latitude;
 
-//making Google Maps variables
+//Google Maps variables
 var map;
 var service;
 var infowindow;
+var breweryName;
 
 //fetch(url).then(response => response.json()).then(responseJson => renderMap(responseJson));
 
-function renderMap(userInput) {
-
-    //from https://developers-dot-devsite-v2-prod.appspot.com/maps/documentation/javascript/examples/place-search
-    
+function initializeMap() {
     var breweryLocation = new google.maps.LatLng(41.8870058, -87.6722122);
 
     infowindow = new google.maps.InfoWindow();
 
     map = new google.maps.Map(
         document.getElementById('map'), {center: breweryLocation, zoom: 15});
+}
+
+
+function renderMap(userInput) {
+    //from https://developers-dot-devsite-v2-prod.appspot.com/maps/documentation/javascript/examples/place-search
+    
+    /*var breweryLocation = new google.maps.LatLng(41.8870058, -87.6722122);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: breweryLocation, zoom: 15});*/
 
     var request = {
         query: `Breweries near ${userInput}`,
@@ -45,7 +54,9 @@ function renderMap(userInput) {
     service = new google.maps.places.PlacesService(map);
 
     service.findPlaceFromQuery(request, function(results, status) {
+        breweryName = results[0].name;
         console.log(results);
+        //console.log(breweryName);
         if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
@@ -55,8 +66,17 @@ function renderMap(userInput) {
         }
     });
       
+    let url = buildUrl(breweryName);
+    console.log(url);
+    fetch(url).then(response => response.json()).then(responseJson => console.log(responseJson));
+}
 
-      
+function buildUrl(breweryName) {
+    //this function is responsible for building the API call URL
+    let baseUrl = `https://api.openbrewerydb.org/breweries`;
+    let noSpaceString = breweryName.replace(/ /g, "%20");
+    let url = baseUrl + "?by_name=" + noSpaceString;
+    return url;
 }
 
 function createMarker(place) {
