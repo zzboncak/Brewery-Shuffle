fe8add9d5670bc84b860ef7a1cc3d448789cbdf7
Zachary <zachary.zboncak@gmail.com>
2019-10-15 21:23:12 -0500

    sped up query time

--- index2.js
+++ index2.js
@@ -15,25 +15,25 @@ function watchForm() {
             alert(`Please enter a State and Zip`);
         }else{
             //this is where the API will be called
-            getStateBreweries(userState);
+            getStateBreweries(userState, userCity);
         }
     })
 }
 
 //the API only allows the user to call 50 breweries at a time. These two functions iterates through
 //all the pages until it's done.
-function getStateBreweries(userState, page=1) {
-    let baseUrl = `https://api.openbrewerydb.org/breweries?by_state=`;
-    let url = baseUrl + userState + `&page=${page}&per_page=50`;
-    fetch(url).then(response => response.json()).then(responseJson => logBreweries(responseJson, userState, page));
+function getStateBreweries(userState, userCity, page=1) {
+    let baseUrl = `https://api.openbrewerydb.org/breweries?by_state=${userState}&by_city=${userCity}`;
+    let url = baseUrl + `&page=${page}&per_page=50`;
+    fetch(url).then(response => response.json()).then(responseJson => logBreweries(responseJson, userState, userCity, page));
 }
 
 //if the response is less than 50 results, then that's all the breweries that are left
 //if there are exactly 50, there may be more, so it calls the API again with an increase of 1 in the page number
-function logBreweries(responseJson, userState, page) {
+function logBreweries(responseJson, userState, userCity, page) {
     if (responseJson.length < 50) {
         Array.prototype.push.apply(breweries, responseJson);
-        console.log(breweries);
+        //console.log(breweries);
         
         //custom event for executing other functions once all the API calls are done
         let event = new Event('doneCalling');
@@ -44,7 +44,7 @@ function logBreweries(responseJson, userState, page) {
     }else if (responseJson.length = 50) {
         Array.prototype.push.apply(breweries, responseJson);
         let newPage = page + 1;
-        getStateBreweries(userState, newPage);
+        getStateBreweries(userState, userCity, newPage);
     }
 }
 
